6e719cb0bd1cd36d7d0bc0887dc5c52f6395ac67
novastosha <galliigotu@gmail.com>
2021-01-27 14:45:10 +0100

    Added Cuboid positions of waiting area for later use, added waiting spawn location field, modified constructor parameters, added get and set methods for different fields.

--- src/dev/nova/skywars/arena/FinalArena.java
+++ src/dev/nova/skywars/arena/FinalArena.java
@@ -1,6 +1,9 @@
 package dev.nova.skywars.arena;
 
+import org.bukkit.Location;
 import org.bukkit.World;
+
+import java.util.List;
 
 public class FinalArena implements Cloneable {
 
@@ -8,12 +11,20 @@ public class FinalArena implements Cloneable {
     private final String fancyName;
     private final World world;
     private final int maxPlayers;
+    private final Location waitingArea;
+    private final Location waitingAreaP1;
+    private final Location waitingAreaP2;
+    private final List<Location> cages;
 
-    public FinalArena(String codeName, String displayName, int maxPlayers,World world) {
+    public FinalArena(String codeName, String displayName, int maxPlayers, List<Location> cages, World world, Location waiting, Location waitingAreaP1, Location waitingAreaP2) {
         this.codeName = codeName;
         this.fancyName = displayName;
         this.maxPlayers = maxPlayers;
         this.world = world;
+        this.waitingArea = waiting;
+        this.waitingAreaP1 = waitingAreaP1;
+        this.waitingAreaP2 = waitingAreaP2;
+        this.cages =cages;
     }
 
     public String getCodeName() {
@@ -44,5 +55,21 @@ public class FinalArena implements Cloneable {
 
     public World getWorld() {
         return world;
+    }
+
+    public Location getWaitingArea() {
+        return waitingArea;
+    }
+
+    public Location getWaitingAreaP1() {
+        return waitingAreaP1;
+    }
+
+    public Location getWaitingAreaP2() {
+        return waitingAreaP2;
+    }
+
+    public List<Location> getCages() {
+        return cages;
     }
 }
