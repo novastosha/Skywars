59bdd22c82e4565afb7a42f1bc37be55c34d7d5d
novastosha <galliigotu@gmail.com>
2021-02-02 07:48:25 +0100

    Re-created the cage system... (Awaiting improvement...)

--- src/dev/nova/skywars/cages/Cage.java
+++ src/dev/nova/skywars/cages/Cage.java
@@ -1,18 +1,105 @@
 package dev.nova.skywars.cages;
 
-import com.sun.javafx.collections.ArrayListenerHelper;
-import dev.nova.skywars.player.SkyWarsPlayer;
 import org.bukkit.Location;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.Material;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Cage {
 
-    private final ArrayList<Location> locations;
+    private final HashMap<String, Material> locations;
 
-    public Cage(ArrayList<Location> locations){
-       this.locations = locations;
+    public Cage(HashMap<String, Material> locations) {
+        this.locations = locations;
     }
 
+    public void paste(Location location) {
+        location = new Location(location.getWorld(),location.getBlockX(),location.getBlockY(),location.getBlockZ());
+        for(String stringLocation : locations.keySet()){
+            String[] xyz = stringLocation.split("_");
+
+            int pasteX = 0,pasteY = 0,pasteZ = 0;
+            Location pasteLocation;
+
+            int inCoord = 0;
+            for(String xoyoz : xyz){
+
+                if(xoyoz.contains("+")){
+                    switch (inCoord){
+                        case 0:
+                            pasteX = Integer.parseInt(xoyoz.split("\\+")[1]);
+                            break;
+                        case 1:
+                            pasteY = Integer.parseInt(xoyoz.split("\\+")[1]);
+                            break;
+                        case 2:
+                            pasteZ = Integer.parseInt(xoyoz.split("\\+")[1]);
+                            break;
+                    }
+                }else if(xoyoz.contains("-")) {
+                    switch (inCoord) {
+                        case 0:
+                            pasteX = -Integer.parseInt(xoyoz.split("-")[1]);
+                            break;
+                        case 1:
+                            pasteY = -Integer.parseInt(xoyoz.split("-")[1]);
+                            break;
+                        case 2:
+                            pasteZ = -Integer.parseInt(xoyoz.split("-")[1]);
+                            break;
+                    }
+                }
+                inCoord++;
+            }
+
+            pasteLocation = new Location(location.getWorld(), location.getBlockX()+pasteX,location.getBlockY()+pasteY,location.getBlockZ()+pasteZ);
+            pasteLocation.getWorld().getBlockAt(pasteLocation).setType(locations.get(stringLocation));
+            location = new Location(location.getWorld(),location.getBlockX(),location.getBlockY(),location.getBlockZ());
+        }
+    }
+
+    public void remove(Location location) {
+        location = new Location(location.getWorld(),location.getBlockX(),location.getBlockY(),location.getBlockZ());
+        for(String stringLocation : locations.keySet()){
+            String[] xyz = stringLocation.split("_");
+
+            int pasteX = 0,pasteY = 0,pasteZ = 0;
+            Location pasteLocation;
+
+            int inCoord = 0;
+            for(String xoyoz : xyz){
+
+                if(xoyoz.contains("+")){
+                    switch (inCoord){
+                        case 0:
+                            pasteX = Integer.parseInt(xoyoz.split("\\+")[1]);
+                            break;
+                        case 1:
+                            pasteY = Integer.parseInt(xoyoz.split("\\+")[1]);
+                            break;
+                        case 2:
+                            pasteZ = Integer.parseInt(xoyoz.split("\\+")[1]);
+                            break;
+                    }
+                }else if(xoyoz.contains("-")) {
+                    switch (inCoord) {
+                        case 0:
+                            pasteX = -Integer.parseInt(xoyoz.split("-")[1]);
+                            break;
+                        case 1:
+                            pasteY = -Integer.parseInt(xoyoz.split("-")[1]);
+                            break;
+                        case 2:
+                            pasteZ = -Integer.parseInt(xoyoz.split("-")[1]);
+                            break;
+                    }
+                }
+                inCoord++;
+            }
+
+            pasteLocation = new Location(location.getWorld(), location.getBlockX()+pasteX,location.getBlockY()+pasteY,location.getBlockZ()+pasteZ);
+            pasteLocation.getWorld().getBlockAt(pasteLocation).setType(Material.AIR);
+            location = new Location(location.getWorld(),location.getBlockX(),location.getBlockY(),location.getBlockZ());
+        }
+    }
 }
